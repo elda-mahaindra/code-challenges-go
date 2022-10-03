f1afbb5ea57ac8704570922e361d2642f5aa14a6
elda-mahaindra <faith030@gmail.com>
2022-10-03 18:03:43 +0700

    feat: challenge 001
    Add challenge 001 - `Most Repeated Character`.

---
+++ challenges/challenges_001/challenge_001.go
@@ -0,0 +1,113 @@
+/*
+   Most Repeated Character
+
+   Given a string 's', return the most frequent character (an alphabet letter) inside string 's'.
+
+   Constraints:
+     • 1 <= s.length <= 100
+     • s contains lower-case and upper-case English letters and numbers
+
+   Example 1:
+     Input: s = "abcddefda1111133333333"
+     Output: 'd'
+
+   Example 2:
+     Input: s= "AA0AB0BB0ccc0aa0aw00wo0BBBw123123"
+     Output: 'B'
+
+   source: turing
+*/
+
+package challenges_001
+
+import (
+	"errors"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+const (
+	ALPHABET_NOT_FOUND string = "the string input 's' should contain at least one alphabet letter"
+	OUT_OF_RANGE_S     string = "the length of input 's' should be between 1 and 100"
+)
+
+type status struct {
+	char      string
+	occurence int32
+}
+
+func isValid(s string) error {
+
+	switch {
+	case len(s) < 1 || len(s) > 100:
+		return errors.New(OUT_OF_RANGE_S)
+	case !func(s string) bool {
+		splitted := strings.Split(s, "")
+
+		containAlphabet := false
+		for _, letter := range splitted {
+			regex, err := regexp.Compile("[a-zA-Z]")
+			if err != nil {
+				containAlphabet = containAlphabet && false
+			}
+
+			containAlphabet = containAlphabet || regex.MatchString(letter)
+		}
+
+		return containAlphabet
+	}(s):
+		return errors.New(ALPHABET_NOT_FOUND)
+	default:
+		return nil
+	}
+}
+
+func Solution(s string) (string, error) {
+	err := isValid(s)
+	if err != nil {
+		return "", err
+	}
+
+	splitted := strings.Split(s, "")
+	sort.Strings(splitted)
+
+	statuses := []status{}
+	for i := 0; i < len(splitted); i++ {
+		regex, err := regexp.Compile("[a-zA-Z]")
+		if err != nil {
+			continue
+		}
+
+		if !regex.MatchString(splitted[i]) {
+			continue
+		}
+
+		if len(statuses) == 0 {
+			statuses = append(statuses, status{
+				char:      splitted[i],
+				occurence: 1,
+			})
+		} else {
+			latest := statuses[len(statuses)-1]
+			if splitted[i] == latest.char {
+				statuses = statuses[0 : len(statuses)-1]
+				statuses = append(statuses, status{
+					char:      splitted[i],
+					occurence: latest.occurence + 1,
+				})
+			} else {
+				statuses = append(statuses, status{
+					char:      splitted[i],
+					occurence: 1,
+				})
+			}
+		}
+	}
+
+	sort.SliceStable(statuses, func(i, j int) bool {
+		return statuses[i].occurence > statuses[j].occurence
+	})
+
+	return statuses[0].char, nil
+}
---
+++ challenges/challenges_001/challenge_001_test.go
@@ -0,0 +1,77 @@
+package challenges_001_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	challenge "code-challenges-go/challenges/challenges_001"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestChallenge_001(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		s string
+	}
+
+	successCases := []struct {
+		input  input
+		output string
+	}{
+		{
+			input:  input{s: "abcddefda1111133333333"},
+			output: "d",
+		},
+		{
+			input:  input{s: "AA0AB0BB0ccc0aa0aw00wo0BBBw123123"},
+			output: "B",
+		},
+		{
+			input:  input{s: "aaaaaaaaaaaBCGDhsjwnq"},
+			output: "a",
+		},
+	}
+
+	failureCases := []struct {
+		input input
+		err   error
+	}{
+		{
+			input: input{s: "0000000012312313123"},
+			err:   errors.New(challenge.ALPHABET_NOT_FOUND),
+		},
+		{
+			input: input{s: "zvchW39Kt5mQQLXXE8IekkBAVOjwRVaKaHGwSI44uuJ005n1luXnfwW8pBW7S1ujXdrintWaZ7nK6dBsjch4BRPYazB2kdcAgfZak"},
+			err:   errors.New(challenge.OUT_OF_RANGE_S),
+		},
+	}
+
+	for i := range successCases {
+		tc := successCases[i]
+
+		t.Run(fmt.Sprintf("success case %d", i), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := challenge.Solution(tc.input.s)
+			require.NoError(t, err)
+			require.NotNil(t, result)
+			require.Equal(t, tc.output, result)
+		})
+	}
+
+	for i := range failureCases {
+		tc := failureCases[i]
+
+		t.Run(fmt.Sprintf("failure case %d", i), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := challenge.Solution(tc.input.s)
+			require.Error(t, err)
+			require.Equal(t, "", result)
+			require.Equal(t, tc.err, err)
+		})
+	}
+}
